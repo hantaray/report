3ec6f556a2c16a0e75ce47f50981ae7d37019ebf
hantaray <info@alexander-hantke.com>
2025-02-14 21:17:16 +0100

    Create routes

---
+++ backend/app/routes.py
@@ -0,0 +1,30 @@
+from flask import request, jsonify
+from app import db
+from app.models import User, Report
+from flask_jwt_extended import create_access_token
+
+def register_routes(app):
+    @app.route('/register', methods=['POST'])
+    def register():
+        data = request.get_json()
+        new_user = User(username=data['username'], password=data['password'])
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"message": "User registered successfully"}), 201
+
+    @app.route('/login', methods=['POST'])
+    def login():
+        data = request.get_json()
+        user = User.query.filter_by(username=data['username']).first()
+        if user and user.password == data['password']:
+            access_token = create_access_token(identity={'username': user.username})
+            return jsonify(access_token=access_token), 200
+        return jsonify({"message": "Invalid credentials"}), 401
+
+    @app.route('/report', methods=['POST'])
+    def report():
+        data = request.get_json()
+        new_report = Report(description=data['description'], user_id=data['user_id'])
+        db.session.add(new_report)
+        db.session.commit()
+        return jsonify({"message": "Report submitted successfully"}), 201
