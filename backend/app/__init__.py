bf6e8fe2d6565f987564c89ca0bb6a5bda2954ff
hantaray <info@alexander-hantke.com>
2025-02-14 21:12:57 +0100

    Create init

---
+++ backend/app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from app.config import Config
+
+db = SQLAlchemy()
+jwt = JWTManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    jwt.init_app(app)
+
+    with app.app_context():
+        from app import routes
+        db.create_all()
+
+    return app
